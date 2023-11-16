fb042b9aac6adb58b17b0d817f7c320e73c1b58d
Melvin Viloria <melvinviloria206@gmail.com>
2023-11-17 08:47:27 +1300

    Add project files.

---
+++ FoodDevApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodDevApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
