2342a179fb76773ce9d49950b744b9a090d531bd
RonaldLK21 <rony.lakatos15@outlook.com>
2023-11-24 10:59:50 +1300

    Login Page and SignUp Page

--- FoodDevApp/App.xaml.cs
+++ FoodDevApp/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace FoodDevApp
-{
+﻿namespace FoodDevApp;
+
+using FoodDeliveryApp.MVVM.View;
+using FoodDevApp.MVVM.View;
+
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new startup());
+            MainPage = new NavigationPage(new LogInPage());
         }
     }
-}
--- FoodDevApp/MVVM/View/LoginPage.xaml.cs
+++ FoodDevApp/MVVM/View/LoginPage.xaml.cs
@@ -1,9 +1,27 @@
-namespace FoodDevApp.MVVM.View;
+using FoodDevApp;
 
-public partial class LoginPage : ContentPage
+namespace FoodDeliveryApp.MVVM.View;
+
+public partial class LogInPage : ContentPage
 {
-	public LoginPage()
+	public LogInPage()
 	{
 		InitializeComponent();
 	}
+
+    private void SignUp_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new SignUpPage());
+    }
+
+    private void ForgotPass_Clicked(object sender, EventArgs e)
+    {
+       
+
+    }
+
+    private void Login_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new startup());
+    }
 }
