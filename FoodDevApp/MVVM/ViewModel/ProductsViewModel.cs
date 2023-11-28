3d43e72fa36d77c9a75d9b1092d035d2ef74cf6b
Melvin Viloria <melvinviloria206@gmail.com>
2023-11-28 17:57:03 +1300

    fixed merge errors and fixed appshell ez

--- FoodDevApp/MVVM/ViewModel/ProductsViewModel.cs
+++ FoodDevApp/MVVM/ViewModel/ProductsViewModel.cs
@@ -1,135 +1,122 @@
 ﻿using FoodDevApp.MVVM.Model;
-using IntelliJ.Lang.Annotations;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 using FoodDevApp.MVVM.Data;
-using FoodDevApp.MVVM.Model;
+using FoodDevApp;
 using FoodDevApp.MVVM.ViewModel;
 
 namespace FoodDevApp.MVVM.ViewModel
-{ 
-    internal partial class ProductsViewModel : ObservableObject
 {
-    private readonly DataBaseContext _context;
-
-    public ProductsViewModel(DataBaseContext context)
+    internal partial class ProductsViewModel : Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject
     {
-        _context = context;
-    }
-    [ObservableProperty]
-    private ObservableCollection<Product> _products;
+        private readonly DataBaseContext _context;
 
-    [ObservableProperty]
-    private Product _operatingProduct = new();
-
-    [ObservableProperty]
-    private bool _isBusy;
-
-    [ObservableProperty]
-    private string _busyText;
-
-    [RelayCommand]
-    private async Task LoadProductsAsync()
-    {
-
-        await ExecuteAsync(async () =>
+        public ProductsViewModel(DataBaseContext context)
         {
-            var products = await _context.GetAllAsync<Product>();
+            _context = context;
+        }
 
-            if (products is not null && products.Any())
+        [Microsoft.Toolkit.Mvvm.ComponentModel.ObservableProperty]
+        private ObservableCollection<Product> _products;
+
+        [Microsoft.Toolkit.Mvvm.ComponentModel.ObservableProperty]
+        private Product _operatingProduct = new();
+
+        [Microsoft.Toolkit.Mvvm.ComponentModel.ObservableProperty]
+        private bool _isBusy;
+
+        [Microsoft.Toolkit.Mvvm.ComponentModel.ObservableProperty]
+        private string _busyText;
+
+        [RelayCommand]
+        private async Task LoadProductsAsync()
+        {
+            await ExecuteAsync(async () =>
             {
-                Products ??= new ObservableCollection<Product>();
+                var products = await _context.GetAllAsync<Product>();
 
-                foreach (var product in products)
+                if (products is not null && products.Any())
                 {
-                    Products.Add(product);
+                    Products ??= new ObservableCollection<Product>();
+
+                    foreach (var product in products)
+                    {
+                        Products.Add(product);
+                    }
+                }
+            }, "fetching Products");
+        }
+
+        [RelayCommand]
+        private void SetOperatingProducy(Product? product) => OperatingProduct = product ?? new();
+
+        [RelayCommand]
+        private async Task SaveProductAsync()
+        {
+            if (OperatingProduct is null)
+                return;
+
+            var busyText = OperatingProduct.Id == 0 ? "creating Product...." : "Updating product";
+
+            await ExecuteAsync(async () =>
+            {
+                if (OperatingProduct.Id == 0)
+                {
+                    await _context.AdditemAsync<Product>(OperatingProduct);
+                    Products.Add(OperatingProduct);
+                }
+                else
+                {
+                    await _context.UpdateitemAsync<Product>(OperatingProduct);
+
+                    var ProductCopy = OperatingProduct.Clone();
+
+                    var index = Products.IndexOf(OperatingProduct);
+                    Products.RemoveAt(index);
+
+                    Products.Insert(index, ProductCopy);
                 }
 
-            }
-        }, "fetching Products");
-
-    }
-
-
-    [RelayCommand]
-    private void SetOperatingProducy(Product? product) => OperatingProduct = product ?? new();
-
-
-
-    [RelayCommand]
-    private async Task SaveProductAsync()
-    {
-        if (OperatingProduct is null)
-            return;
-
-        var busyText = OperatingProduct.Id == 0 ? "creating Product...." : "Updating product";
-
-        await ExecuteAsync(async () =>
-        {
-            if (OperatingProduct.Id == 0)
-            {
-                await _context.AdditemAsync<Product>(OperatingProduct);
-                Products.Add(OperatingProduct);
-            }
-            else
-            {
-                await _context.UpdateitemAsync<Product>(OperatingProduct);
-
-                var ProductCopy = OperatingProduct.Clone();
-
-                var index = Products.IndexOf(OperatingProduct);
-                Products.RemoveAt(index);
-
-                Products.Insert(index, ProductCopy);
-            }
-
-
-
-            setOperatingProducyCommand.Execute(new());
-        }, BusyText);
-    }
-
-
-    [RelayCommand]
-    private async Task DeleteProductAsync(int Id)
-    {
-        await ExecuteAsync(async () =>
-        {
-            if (await _context.DeleteitemByKeyAsync<Product>(Id))
-            {
-                var product = Products.FirstOrDefault(p => p.Id == Id);
-                Products.Remove(product);
-            }
-            else
-            {
-                await Shell.Current.DisplayAlert("Delete Errorr", "Product not deleted", "Ok");
-            }
-        }, "Deleting Product");
-
-    }
-
-    private async Task ExecuteAsync(Func<Task> operation, string? busytext = null)
-    {
-        IsBusy = true;
-        BusyText = busytext ?? "Processing ..........";
-        try
-        {
-            await operation?.Invoke();
-        }
-        finally
-        {
-            IsBusy = false;
-            BusyText = "Processing ..........";
+                setOperatingProducyCommand.Execute(new());
+            }, BusyText);
         }
 
+        [RelayCommand]
+        private async Task DeleteProductAsync(int Id)
+        {
+            await ExecuteAsync(async () =>
+            {
+                if (await _context.DeleteitemByKeyAsync<Product>(Id))
+                {
+                    var product = Products.FirstOrDefault(p => p.Id == Id);
+                    Products.Remove(product);
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Delete Errorr", "Product not deleted", "Ok");
+                }
+            }, "Deleting Product");
+        }
+
+        private async Task ExecuteAsync(Func<Task> operation, string? busytext = null)
+        {
+            IsBusy = true;
+            BusyText = busytext ?? "Processing ..........";
+            try
+            {
+                await operation?.Invoke();
+            }
+            finally
+            {
+                IsBusy = false;
+                BusyText = "Processing ..........";
+            }
+        }
     }
-}
 }
